7cb94e386106c26d3d7920d4496e7e8fc9bfc19d
duaraghav8 <duaraghav8@gmail.com>
2017-12-13 21:56:20 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * @fileoverview Entry Point of Embark Framework Solium Plugin
+ * @author Raghav Dua <duaraghav8@gmail.com>
+ */
+
+"use strict";
+
+
+function handler(Embark) {
+    Embark.events.on("file-changed", (fileType, path) => {
+        if (fileType === "contract") {
+            Embark.logger.info(`File changed: ${path}`);
+        }
+
+        Embark.logger.info(`Plugin configuration:\n${JSON.stringify(Embark.pluginConfig, null, 4)}`);
+    });
+}
+
+
+module.exports = handler;
