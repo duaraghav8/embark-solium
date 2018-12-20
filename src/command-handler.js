496d6be05cd5d43c7398c09a47dc6dbbe34ee13a
emizzle <ericmastro@status.im>
2018-12-20 19:44:10 +1100

    Support for embark 4-alpha.2
    There were a few changes and deprecations in embark 4 that have been addressed.

--- src/command-handler.js
+++ src/command-handler.js
@@ -9,16 +9,6 @@
 const Solium = require("solium"), fs = require("fs"),
     consts = require("./constants"), path = require("path"), Table = require("text-table");
 
-
-function tokenize(command) {
-    const tokens = command
-        .split(" ")
-        .map(t => t.trim())
-        .filter(t => t.length > 0);
-
-    return tokens;
-}
-
 function createConfigFiles() {
     const defaultConfig = Solium.getDefaultConfig(),
         soliumrc = path.join(consts.ROOT_DIR, consts.SOLIUMRC_JSON),
@@ -34,38 +24,38 @@ function help() {
 
 
 // eslint-disable-next-line no-unused-vars
-module.exports = (Embark, command, options) => {
+module.exports = {
+    description: "Interacts with the solium plugin for embark.",
+    matches: (cmd) => {
+        const [commandName] = cmd.split(" ");
+        return commandName && commandName.toLowerCase() === "solium";
+    },
+    usage: help(),
+    process: (cmd, callback) => {
+        const [, option] = cmd.split(" ");
+        if (!option) {
+            return callback(null, help());
+        }
 
-    const tokens = tokenize(command);
-
-    return {
-        match: () => tokens[0] === "solium",
-        process: (callback) => {
-            if (tokens.length === 1) {
-                return callback(null, help());
+        /* eslint-disable indent */
+        switch(option) {
+            case "--init":
+            case "-i":
+            try {
+                createConfigFiles();
+            } catch (e) {
+                return callback(null, `Error while creating Solium config files in root dir: ${e.message}`);
             }
 
-            /* eslint-disable indent */
-            switch(tokens[1]) {
-                case "--init":
-                case "-i":
-                try {
-                    createConfigFiles();
-                } catch (e) {
-                    return callback(null, `Error while creating Solium config files in root dir: ${e.message}`);
-                }
+            return callback(null, `Created ${consts.SOLIUMRC_JSON} & ${consts.SOLIUMIGNORE} in root directory.`);
 
-                return callback(null, `Created ${consts.SOLIUMRC_JSON} & ${consts.SOLIUMIGNORE} in root directory.`);
+            case "--help":
+            case "-h":
+                return callback(null, help());
 
-                case "--help":
-                case "-h":
-                    return callback(null, help());
-
-                default:
-                    return callback(null, `Invalid option "${tokens[1]}".\n${help()}`);
-          }
-          /* eslint-enable indent */
+            default:
+                return callback(null, `Invalid option "${option}".\n${help()}`);
         }
-    };
-
+        /* eslint-enable indent */
+    }
 };
